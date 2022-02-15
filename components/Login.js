58f3ece218f871b8bcc6d167d27521bd59e6a1f7
zilhamdev <zilhamdev@gmail.com>
2022-02-15 13:21:24 +0700

    add login function web3

---
+++ components/Login.js
@@ -0,0 +1,37 @@
+import react from 'react';
+import Image from 'next/image';
+import { useMoralis } from 'react-moralis';
+
+function Login() {
+    const { authenticate } = useMoralis();
+    return (
+        <div className='bg-black relative'>
+            <h1>Login Screen</h1>
+            <div className='flex flex-col absolute z-50 h-4/6 justify-center items-center w-full space-y-4'>
+                {/* Metaverse Logo */}
+                <Image src='https://links.papareact.com/3pi'
+                    className='object-cover rounded-full'
+                    height={200}
+                    width={200}
+                    alt='metaverse logo'
+                />
+
+                {/* Login Button */}
+                <button
+                    className='bg-yellow-400 hover:bg-yellow-500 rounded-lg p-3 animate-pulse hover:animate-none font-bold'
+                    onClick={() => authenticate()}
+                >Login to the METAVERSE</button>
+            </div>
+            <div className='w-full h-screen'>
+                <Image src='https://links.papareact.com/55n'
+                    layout='fill'
+                    objectFit='cover'
+                    alt='metaverse background'
+                />
+                {/* Login Form */}
+            </div>
+        </div>
+    )
+}
+
+export default Login
